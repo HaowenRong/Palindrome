15ed72ca1c4182058b0f1d5b0dc01d53db316400
Haowen Rong <Alexrong1116@gmail.com>
2023-09-08 18:59:56 +0100

    Created initial code for program

---
+++ palindrome.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Palindrome {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a string");
+
+    String palindrome = scanner.nextLine();
+    // call palendrome function
+  }
+}
+
+public boolean isPalendrome(String palindrome) {
+
+}
