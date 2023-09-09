d104efdd81ec5a9b55c33786044bc8a7eba8a799
Haowen Rong <Alexrong1116@gmail.com>
2023-09-09 18:31:10 +0100

    Completed function to detect palindrome which displays its process and outcome

--- palindrome.java
+++ palindrome.java
@@ -1,15 +1,33 @@
 import java.util.Scanner;
 
-public class Palindrome {
+public class Main {
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter a string");
+    while(true) {
+      System.out.println("Enter a string");
 
-    String palindrome = scanner.nextLine();
-    // call palendrome function
+      String palindrome = scanner.nextLine();
+      System.out.printf("Is \"%s\" a palindrome? %b %n%n", palindrome, isPalindrome(palindrome));
+    }
+
+  }
+  private static boolean isPalindrome(String palindrome) {
+    for (int i = 0; i < palindrome.length(); i++) {
+      int currEndChar = palindrome.length() - i - 1;
+      
+      System.out.printf("+%d %c | -%d %c %n", (i + 1), palindrome.charAt(i), (currEndChar + 1), palindrome.charAt(currEndChar));
+      
+      // if the char index become the same or cross eachother
+      if (i == currEndChar || i > currEndChar) {
+        break;
+      }
+      
+      // return if characters are not the same
+      if (palindrome.charAt(i) != palindrome.charAt(currEndChar)) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
-public boolean isPalendrome(String palindrome) {
-
-}
